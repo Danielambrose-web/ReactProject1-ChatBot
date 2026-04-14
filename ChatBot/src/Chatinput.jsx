7f72c3eda60d91608bce049ea9fea46cfb73fe8a
Danielambrose-web <danielessientech121@gmail.com>
2026-04-14 12:05:04 +0100

    created chat input

---
+++ ChatBot/src/Chatinput.jsx
@@ -0,0 +1,11 @@
+    
+    function ChatInput() {
+        return (
+          <>
+            <input placeholder="send message" size="50" />
+            <button>Send</button>
+          </>
+        );
+      }
+
+      export default ChatInput
