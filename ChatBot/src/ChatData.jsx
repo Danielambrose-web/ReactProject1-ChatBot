5b8289609f7c04caeda34faa1f8a916ade6efb9c
Danielambrose-web <danielessientech121@gmail.com>
2026-04-22 11:16:15 +0100

    menn  run into serious trouble

--- ChatBot/src/ChatData.jsx
+++ ChatBot/src/ChatData.jsx
@@ -1,23 +1,29 @@
 import ChatMessage from "./ChatMessage.jsx";
+import React, { useState } from "react";
 
 function ChatData() {
-  const messages = [
+  const array = useState([
     { message: "hi", sender: "user", key: "mes1" },
     { message: "hi", sender: "robot", key: "mes2" },
     { message: "what is todays date", sender: "user", key: "mes3" },
     { message: "date is april", sender: "robot", key: "mes4" },
     { message: "wow what is ai", sender: "user", key: "mes5" },
     { message: "it is an assistant", sender: "robot", key: "mes6" },
-  ];
+  ]);
+  const [Messages, setMessages] = array;
 
+ 
+
+  function sendMessage() {
+ 
+
+  }
   return (
     <div>
-      {messages.map((msg) => (
-        <ChatMessage 
-        key={msg.key} 
-        sender={msg.sender} 
-        message={msg.message}
-         />
+      <button onClick={sendMessage}>Send Message</button>
+
+      {Messages.map((msg) => (
+        <ChatMessage key={msg.key} sender={msg.sender} message={msg.message} />
       ))}
     </div>
   );
--- ChatBot/src/Chatinput.jsx
+++ ChatBot/src/Chatinput.jsx
@@ -1,11 +1,20 @@
-    
-    function ChatInput() {
-        return (
-          <>
-            <input placeholder="send message" size="50" />
-            <button>Send</button>
-          </>
-        );
-      }
+import Messages from "./ChatData.jsx";
+import React, { useState } from "react";
 
-      export default ChatInput
+function ChatInput() {
+  const [inputText, setInputText] = useState("");
+  function saveTextInput(input) {
+    setInputText(input.target.value);
+  }
+  function sendMessage() {
+    console.log(inputText);
+  }
+  return (
+    <>
+      <input placeholder="send message" size="50" onChange={saveTextInput} />
+      <button onClick={sendMessage}>Send</button>
+    </>
+  );
+}
+
+export default ChatInput;
