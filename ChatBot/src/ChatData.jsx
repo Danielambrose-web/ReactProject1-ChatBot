7327edeff365305c1e31c4e1af78776af7cad7c2
Danielambrose-web <danielessientech121@gmail.com>
2026-04-22 22:19:55 +0100

    deleted the state from chatdata

--- ChatBot/src/ChatData.jsx
+++ ChatBot/src/ChatData.jsx
@@ -1,27 +1,11 @@
-import ChatMessage from "./ChatMessage.jsx";
-import React, { useState } from "react";
 
-function ChatData() {
-  const array = useState([
-    { message: "hi", sender: "user", key: "mes1" },
-    { message: "hi", sender: "robot", key: "mes2" },
-    { message: "what is todays date", sender: "user", key: "mes3" },
-    { message: "date is april", sender: "robot", key: "mes4" },
-    { message: "wow what is ai", sender: "user", key: "mes5" },
-    { message: "it is an assistant", sender: "robot", key: "mes6" },
-  ]);
-  const [Messages, setMessages] = array;
+import App from "./App.jsx";
+import ChatMessage from "./ChatMessage"; 
 
- 
 
-  function sendMessage() {
- 
-
-  }
+function ChatData({ Messages }) {
   return (
     <div>
-      <button onClick={sendMessage}>Send Message</button>
-
       {Messages.map((msg) => (
         <ChatMessage key={msg.key} sender={msg.sender} message={msg.message} />
       ))}
