4f59b32c96fdd3a441226d1e319cbed3957b13ca
Danielambrose-web <danielessientech121@gmail.com>
2026-04-22 22:19:26 +0100

    move the state from chatdata to app to make it reusable

--- ChatBot/src/App.jsx
+++ ChatBot/src/App.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useState } from "react";
+import ChatData from "./ChatData";
+import ChatInput from "./ChatInput";
 
-const App = () => {
+function App() {
+  const [Messages, setMessages] = useState([
+    { message: "hi", sender: "user", key: "mes1" },
+    { message: "hi", sender: "robot", key: "mes2" },
+    { message: "what is todays date", sender: "user", key: "mes3" },
+    { message: "date is april", sender: "robot", key: "mes4" },
+    { message: "wow what is ai", sender: "user", key: "mes5" },
+    { message: "it is an assistant", sender: "robot", key: "mes6" },
+  ]);
+
+  function sendMessage(newText) {
+//  ...
+  }
+
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <ChatInput sendMessage={sendMessage} />
+      <ChatData Messages={Messages} />
+    </>
+  );
 }
 
-export default App
+export default App;
